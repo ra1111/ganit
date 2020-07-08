92a9f75038ebb5cce35a1392a418da989b6fba3c
ra1111 <rahula.raj2013@gmail.com>
2020-07-08 21:41:33 +0530

    Updates

---
+++ precache-manifest.7b2b097ac87a914e8806dc462e9da717.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9550719fd39bb665c7198b4b9866fa",
+    "url": "/ra1111/ganit/index.html"
+  },
+  {
+    "revision": "9cf5ebfcfa83f13320e0",
+    "url": "/ra1111/ganit/static/css/main.cfb011fb.chunk.css"
+  },
+  {
+    "revision": "1f4b82eb7ebf694c205e",
+    "url": "/ra1111/ganit/static/js/2.06b56c30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ra1111/ganit/static/js/2.06b56c30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cf5ebfcfa83f13320e0",
+    "url": "/ra1111/ganit/static/js/main.c3e5ced9.chunk.js"
+  },
+  {
+    "revision": "c18550edd800d07ec049",
+    "url": "/ra1111/ganit/static/js/runtime-main.8cc33071.js"
+  },
+  {
+    "revision": "0964be9026881698d4bc5404686fd6f7",
+    "url": "/ra1111/ganit/static/media/images-ai-new.0964be90.png"
+  },
+  {
+    "revision": "957658a691baa19dfe2a40f7ba5ccc99",
+    "url": "/ra1111/ganit/static/media/images-background-new.957658a6.jpg"
+  },
+  {
+    "revision": "7d6a83b16ab7c63383806dc50221b60c",
+    "url": "/ra1111/ganit/static/media/images-banner.7d6a83b1.jpg"
+  },
+  {
+    "revision": "23c9d15ed3e86f71a4326126600e5c66",
+    "url": "/ra1111/ganit/static/media/images-drop-arrow.23c9d15e.svg"
+  },
+  {
+    "revision": "8130e8304a28f324e351c310703d723d",
+    "url": "/ra1111/ganit/static/media/images-happy-coder.8130e830.jpg"
+  },
+  {
+    "revision": "1b158d556c42ca244a7915773a704423",
+    "url": "/ra1111/ganit/static/media/images-header.1b158d55.png"
+  },
+  {
+    "revision": "3916d52e5d439a15e8d2d6a4187c5562",
+    "url": "/ra1111/ganit/static/media/images-icon-10min.3916d52e.svg"
+  },
+  {
+    "revision": "b6bf59e6a5226870362f2aab8f60f3b9",
+    "url": "/ra1111/ganit/static/media/images-icon-20prcnt.b6bf59e6.svg"
+  },
+  {
+    "revision": "2617aaf46a78f587cdb7beb33eefbe91",
+    "url": "/ra1111/ganit/static/media/images-icon-3s.2617aaf4.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/ra1111/ganit/static/media/images-icon-attach.d41d8cd9.svg"
+  },
+  {
+    "revision": "46f818f1e64f56917680b17b45119162",
+    "url": "/ra1111/ganit/static/media/images-icon-x20.46f818f1.svg"
+  },
+  {
+    "revision": "624447c8b120c6dce576bcfe5a7134de",
+    "url": "/ra1111/ganit/static/media/images-man-frame.624447c8.svg"
+  },
+  {
+    "revision": "134f2abea47d23c5885017543a908c5f",
+    "url": "/ra1111/ganit/static/media/images-man-self.134f2abe.png"
+  },
+  {
+    "revision": "151c9aa419d22292da5cc5602e9754f2",
+    "url": "/ra1111/ganit/static/media/images-particles.151c9aa4.svg"
+  },
+  {
+    "revision": "0a51ea5b85c66cb49c503c38febcc810",
+    "url": "/ra1111/ganit/static/media/images-particles2.0a51ea5b.svg"
+  },
+  {
+    "revision": "4cf18bc06973f93d998262ddf970fe79",
+    "url": "/ra1111/ganit/static/media/images-product-bg-mobile.4cf18bc0.jpg"
+  },
+  {
+    "revision": "2c1d3631e5090fe6707d97de43c71d20",
+    "url": "/ra1111/ganit/static/media/images-sprite.2c1d3631.svg"
+  },
+  {
+    "revision": "ee335de5867b023e5bdf70e91c4506e1",
+    "url": "/ra1111/ganit/static/media/images-top-main-bg-320.ee335de5.jpg"
+  },
+  {
+    "revision": "b89ff96a144fc577cc0f9dc28af8abd4",
+    "url": "/ra1111/ganit/static/media/logo.b89ff96a.svg"
+  }
+]);
